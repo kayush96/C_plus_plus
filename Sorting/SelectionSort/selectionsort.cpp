62a9e3e51b6fd0c6e4055f74cd86b809e7fcb9f2
Ayush Kumar <engiworks2016@gmail.com>
2021-08-27 00:53:35 +0530

    Add Selection Sort Program

---
+++ Sorting/SelectionSort/selectionsort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+void selection_sort(int arr[], int n)
+{
+  for (int pos = 0; pos <= n - 2; pos++)
+  {
+    int current = arr[pos];
+    int min_position = pos;
+    //find out the element
+    for (int j = pos; j < n; j++)
+    {
+      if (arr[j] < arr[min_position])
+      {
+        min_position = j;
+      }
+    }
+    swap(arr[min_position], arr[pos]);
+  }
+}
+
+int main()
+{
+  int arr[] = {-2, 3, 4, -1, 5, -12, 6, 1, 3};
+  int n = sizeof(arr) / sizeof(int);
+
+  selection_sort(arr, n);
+
+  for (auto x : arr)
+  {
+    cout << x << " ";
+  }
+  return 0;
+}
