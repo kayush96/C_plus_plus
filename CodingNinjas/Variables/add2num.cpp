524ed659ab094ff1d40269e99276ea08abb23dd6
Ayush Kumar <engiworks2016@gmail.com>
2021-08-31 23:13:10 +0530

    Add variables and data types

--- CodingNinjas/Variables/add2num.cpp
+++ CodingNinjas/Variables/add2num.cpp
@@ -3,10 +3,21 @@ using namespace std;
 
 int main()
 {
+  //Integer
   int a, b, sum = 0;
   a = 10;
   b = 20;
   sum = a + b;
   cout << "Sum is: " << sum << endl;
+  //Character
+  char d = 'x';
+  cout << d << endl;
+  //FloatingPoint
+  float f = 1.25;
+  //Boolean
+  bool ab = true;
+
+  int size = sizeof(f);
+  cout << size;
   return 0;
 }
