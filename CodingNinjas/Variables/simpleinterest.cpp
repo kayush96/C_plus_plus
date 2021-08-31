5d702c899a50f3413af846c57f5c4ed9514a2188
Ayush Kumar <engiworks2016@gmail.com>
2021-08-31 23:23:30 +0530

    Add Simple Interest Program

---
+++ CodingNinjas/Variables/simpleinterest.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int P, R, T, SI;
+  cin >> P >> R >> T;
+  SI = P * R * T / 100;
+  cout << "Simple Interest is: " << SI << endl;
+  return 0;
+}
