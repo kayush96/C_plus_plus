e5b66c54922243a7794a1f033022b56fd901c8e2
Ayush Kumar <engiworks2016@gmail.com>
2021-09-01 20:15:23 +0530

    Add Pattern 1 program

---
+++ CodingNinjas/Patterns/pattern1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int i = 1, n;
+  cout << "Enter value of n:" << endl;
+  cin >> n;
+
+  while (i <= n)
+  {
+    int j = 1;
+    while (j <= i)
+    {
+      cout << j;
+      j++;
+    }
+    cout << endl;
+    i++;
+  }
+  return 0;
+}
