82715f579bdec4e4a2b645a5cdf4189f8eff87d7
Ayush Kumar <engiworks2016@gmail.com>
2021-09-02 01:25:27 +0530

    Add Pattern 8 program

---
+++ CodingNinjas/Patterns/pattern8.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int i = 1, n;
+  cin >> n;
+  while (i <= n)
+  {
+    int j = 1;
+    while (j <= n)
+    {
+      cout << j;
+      j++;
+    }
+    i++;
+    cout << endl;
+  }
+
+  return 0;
+}
