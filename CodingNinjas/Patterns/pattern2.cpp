bd170be1d731267a7a30077b9784e02c22b956ef
Ayush Kumar <engiworks2016@gmail.com>
2021-09-01 20:15:43 +0530

    Add Pattern 2 program

---
+++ CodingNinjas/Patterns/pattern2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int i = 1, n;
+  cout << "Enter the value of n: " << endl;
+  cin >> n;
+  int val = 1;
+  while (i <= n)
+  {
+    int j = 1;
+    while (j <= i)
+    {
+      cout << val;
+      j++;
+      val++;
+    }
+    i++;
+    cout << endl;
+  }
+  return 0;
+}
